e73fb713e554550907adeec6903395f7be45d7fb
Francisco Vasquez <franjavi4111@gmail.com>
2020-08-04 19:57:56 -0500

    album created

--- src/components/Imagen.jsx
+++ src/components/Imagen.jsx
@@ -5,11 +5,8 @@ import './styles/Imagen.css'
 function Imagen(props) {
     return (
         <div className='imagen'>
-            <div className="imagen__tittle">
-                <h2 className="imaner__tittle-tl">lo que conservamos</h2>
-            </div>
             <div className="imagen__container">
-                <img className='imagen__container-img' src="" alt="imagen"/>
+                <img className='imagen__container-img' src={props.img} alt="imagen"/>
             </div>
             <div className="imagen__descripcion">
                 <p className="imagen__descripcion-desc">descripcion</p>
--- src/containers/Album.jsx
+++ src/containers/Album.jsx
@@ -7,16 +7,39 @@ import './styles/Album.css'
 import Nav from '../components/Nav'
 import Imagen from '../components/Imagen'
 
+//images
+import uno from '../assets/pics/conservamos1.jpeg'
+import dos from '../assets/pics/conservamos2.jpg'
+import tres from '../assets/pics/conservamos3.jpg'
+import cuatro from '../assets/pics/conservamos4.jpeg'
+import quinto from '../assets/pics/conservamos5.jpeg'
+import sexto from '../assets/pics/conservamos6.jpeg'
+import septimo from '../assets/pics/conservamos7.jpeg'
+import octavo from '../assets/pics/conservamos8.jpeg'
+import noveno from '../assets/pics/conservamos9.jpeg'
+
 function Album() {
     return (
         <div className='album'>
             <Nav/>
             <h2 className='album__title'>ALBUM</h2>
+            <p className="album__imagenes-tittle">Lo que conservamos</p>
             <div className="album__imagenes"> 
-                <Imagen/>
-                <Imagen/>
+                <Imagen img={uno}/>
+                <Imagen img={dos}/>
+                <Imagen img={tres}/>
+                <Imagen img={cuatro}/>
+                <Imagen img={quinto}/>
+                <Imagen img={sexto}/>
+                <Imagen img={septimo}/>
+                <Imagen img={octavo}/>
+                <Imagen img={noveno}/>
             </div>
-            
+            {/*viajes incluyendo neusa */}
+
+            {/*peliculas por ver */}
+
+            {/*lugares por visitar*/}
         </div>
     )
 }
