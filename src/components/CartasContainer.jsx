56c1eb15af73bd6d73d98a4e25b02d49e7fa69e7
Francisco Vasquez <franjavi4111@gmail.com>
2020-08-05 13:28:22 -0500

    home page article

--- src/components/CartasContainer.jsx
+++ src/components/CartasContainer.jsx
@@ -6,8 +6,41 @@ export default function CartasContainer() {
         <div className='CartasContainer'>
             <p className='CartasContainer-tittle'>Cumpleaños 2020</p>
             <p className='CartasContainer-article'>
-                una hermosa carta de cumpleñaos
+                <h4>Una hermosa carta de cumpleaños</h4>
+                <article className="CartaContainer__article-text">
+                Es totalmente admirable la persona que eres, has superado 
+                situaciones increiblemente dificil. Creiste muchas veces que te rendias
+                y lo creiste hasta que te levantabas y volvias a iniciar.
+                La mujer que eres hoy tiene cada vez menos que ver con la mujer que eras antes.
+                Cada dia te mejoras a ti misma, cada dia te ves mas y mas feliz y mas y mas fuerte 
+                y conservas esos valores y esa determinacion tuya que te hace ser una hrmosa persona y que
+                muchas veces me hizo decir que eres el tipo de persona que puede mejorar este mundo
+                celebro eso con la misma fuerza que celebro que me ames
+                Pero en estas letras quiero celebrarte a ti
+                porque aunque muchas veces dijiste que no lo hiciera por ti 
+                gracias a ti soy mejor persona, por mas evidente que sea no es facil ver lo malos que somos.
+                Seguramente para ti es aun mas complicado y la razon es simple. Intelectual y moralmente estas muy encima del promedio
+                hablar contigo sobre cualquiero cosa es una inevitable avalancha de conocimiento y observarte pensando en todo lo que eres
+                es una de las pocas cosas que me genera esa sensacion de felicidad completa, y es raro alegrarse tanto y sentirse tan completo al ver
+                a una persona feliz o simplemente ver a esa persona existir, basicamente eres mi felicidad, no hay dia que no quiera 
+                estar a tu lado asi sea solo escuchandote, solo soñando despierto en nuestro futuro mientras te veo comer  
+                </article>
+                <article className="CartaContainer__article-text">
+                Ultimamente cada vez que cumplo años no pienso en que me hago mas viejo, unicamente pienso que estoy mas cerca 
+                de vivir contigo, ese futuro con el que sueño cada dia se comvierte en mi unica deseo y la razon por la cual avanzo cada dia
+                quisiera que pensaras lo mismo. Ahora nos siento mas conectados que nunca y es raro. pasamos por un momento super dificil 
+                y aun asi utilizando unicamente el amor que nos tenemos hemos logrado unirnos de una manera increible esta cuaentena, y si 
+                odiamos el no poder vernos en persona y extrañamos como locos el abrazarnos y besarnos hemos logrado estos meses hacer una enorme 
+                cantidad de cosas juntos, nos abrimos al otro de manera honesta y profunda y juntos cargamos con las dificultades de nuestra situaciones
+                nos amamos infinito y lo seguiremos haciendo hasta viejitos eso ya es un hecho indudable y la mayor preocupacion que tenemos
+                ahora es donde y cuando vamos a vijir juntos, conseguir un empleo para hacer nuestro sueño real, y como vas a lograr que no le ponga a nuestra 
+                futura hija un nombre no tan horrible 
+                Feliz cumpleaños amor de mi vida disfruta mucho este dia disfruta todo lo que puedas. En algun momento el dia de tu cumpleaños
+                me va a pertenecer y sere yo el que se encarge de que ames ese dia 
+                te amo demasiado.
+                </article>
             </p>
+           
         </div>
     )
 }
